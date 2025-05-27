5821dc15c2900c436aec5beb6623ea4f4066e6b8
Kaviarasi Ramamoorthi <kaviarasiramamoorthi04@gmail.com>
2025-05-27 16:20:52 +0530

    nurse dashboard

--- backend/hospital/obj/Debug/net8.0/hospital.AssemblyInfo.cs
+++ backend/hospital/obj/Debug/net8.0/hospital.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hospital")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a73ed58031d43b91ab85c70ab2f68fa96414d1a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beb882c3d83ff9c6ded630daf4e19fd1487bcdf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("hospital")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hospital")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
