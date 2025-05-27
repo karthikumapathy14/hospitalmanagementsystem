fe54c6cd55b4cc8f76d04a6318ce223b357d5ae4
karthikumapathy14 <karthikumapathy14@gmail.com>
2025-05-27 16:19:09 +0530

    nursemodule

--- backend/hospital/obj/Debug/net8.0/hospital.AssemblyInfo.cs
+++ backend/hospital/obj/Debug/net8.0/hospital.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hospital")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46ccf00cb44e3a3ad694c003d393d702015c0c2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8c41a8d8ae9de8936b45eec365de475587d00ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("hospital")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hospital")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
