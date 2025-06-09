4bdc1691d028511757f97ecc3d6e1700e7f61cfd
karthikumapathy14 <karthikumapathy14@gmail.com>
2025-06-09 17:12:46 +0530

    commite

--- backend/hospital/obj/Debug/net8.0/hospital.AssemblyInfo.cs
+++ backend/hospital/obj/Debug/net8.0/hospital.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hospital")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7ef4232405b93c5281a848cfde915c26ae9184d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+428b00d3838c5915e80f211f1a9f34122382ff96")]
 [assembly: System.Reflection.AssemblyProductAttribute("hospital")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hospital")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
